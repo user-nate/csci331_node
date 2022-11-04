73df90385248aeefa8a197bf79069bae9f0f55ab
user-nate <70123268+user-nate@users.noreply.github.com>
2022-11-04 11:28:43 -0600

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/html"});
     response.write("<h1>Hello, Node.js!</h1>");
     response.write("<h2>Nathan Sheffels - j67q156, Node.js!</h2> \
-    <p>Running node server on port 3003</p>");
+    <p>Running node server on port 5003</p> \
+    <p>My repo: <a href='https://github.com/user-nate/csci331_node'>GitHub</a></p>");
     response.end();
-}).listen(3003);
+}).listen(5003);
