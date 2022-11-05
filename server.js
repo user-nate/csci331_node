6984c9416891261d8f3aaf42e4eb364db701cda9
user-nate <54906125+nateMSU@users.noreply.github.com>
2022-11-05 16:47:10 -0600

    hopethisworks

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ http.createServer(function(request, response) {
     response.write("<h2>Nathan Sheffels - j67q156, Node.js!</h2> \
     <p>Running node server on port 5003</p> \
     <p>My repo for this project: <a href='https://github.com/user-nate/csci331_node'>GitHub</a></p> \
-    <p>The repo for my mern 2 project...I have implemented the two components but I could not get the project live on my port :/ <a href='https://github.com/user-nate/mern2'>GitHub</a> </p>");
+    <p>The repo for my mern 2 project...I have implemented the two components, but I could not get the project live on my port :/ <a href='https://github.com/user-nate/mern2'>GitHub</a> </p>");
      response.end();
 }).listen(5003);
